2e1dae321dad860a5861e03492d16491bde89479
Ojus <guptaojas75@gmail.com>
2024-11-29 12:55:58 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,24 @@ const lato = Lato({ subsets: ["latin"], weight: ["400", "700"], variable: "--fon
 const alegreya = Alegreya({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-alegreya" });
 
 export const metadata: Metadata = {
-  title: "Osh's Portfolio",
+  metadataBase: new URL('https://osh-designs.vercel.app'),
+  title: "Osh | Illustrations",
   description: "I create designs that make scientific knowledge clear, engaging, and accessible to a broader audience.",
+  icons: {
+    icon: "favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    title: "Osh | Illustrations",
+    description:
+      "I create designs that make scientific knowledge clear, engaging, and accessible to a broader audience.",
+    siteName: "Osh | Illustrations",
+    locale: "en-US",
+  },
+  twitter: {
+    title: "Osh | Illustrations",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,18 @@ import Portfolio from "@/components/Portfolio";
 import Contact from "@/components/Contact";
 import Process from "@/components/Process";
 
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
-      <Hero />
-      <Portfolio />
-      <AboutSection />
-      <Process />
-      <Contact />
-    </main>
+    <>
+      
+      <main className="min-h-screen bg-background">
+        <Hero />
+        <Portfolio />
+        <AboutSection />
+        <Process />
+        <Contact />
+      </main>
+    </>
   );
 }
